c81b27acccf91e10cdfa052754fa774aec66431e
Tycho <tychosteur@gmail.com>
2024-05-29 22:48:46 +0200

    implemented the counting of syscalls

--- src/syscall_log.c
+++ src/syscall_log.c
@@ -70,8 +70,22 @@ int syscall_log(pid_t tracee_pid, int* status, syscall_table_t* syscall_table) {
 }
 
 int syscall_count_call(pid_t tracee_pid, int* status, syscall_table_t* syscall_table) {
-    (void) tracee_pid;
-    (void) syscall_table;
+    struct user_regs_struct regs;
+    if (tracee_get_regs(tracee_pid, &regs) == -1) {
+        return (TC_ERROR);
+    }
+
+    unsigned long long syscall_num = regs.orig_rax;
+    if (syscall_num >= syscall_table->size)
+        return (TC_ERROR);
+
+    if (syscall_num == SYS_exit || syscall_num == SYS_exit_group) {
+        return (TC_EXIT);
+    }
+
+    syscall_info_t syscall_info = syscall_table->content[syscall_num];
+    syscall_info.calls += 1;
+    
     (void) status;
     return (TC_OK);
 }
